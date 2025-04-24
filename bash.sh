dad49a51a0d94594fb1338c46ee7785bab7996bd
disp06 <59724061+disp06@users.noreply.github.com>
2025-04-24 17:22:28 +0300

    nginx

--- bash.sh
+++
@@ -1,5 +0,0 @@
-echo "FROM php:8.2-apache" > Dockerfile
-echo "COPY src/ /var/www/html/" >> Dockerfile
-echo "RUN a2enmod rewrite" >> Dockerfile
-echo "EXPOSE 80" >> Dockerfile
-echo "CMD [\"apache2-foreground\"]" >> Dockerfile
